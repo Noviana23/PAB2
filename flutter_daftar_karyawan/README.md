c116e6a9e64f36ed392cb18b2b0e9a5396a5c5dd
Noviana23 <noviphi889@gmail.com>
2026-03-04 17:05:51 +0700

    lanjutan

--- flutter_daftar_karyawan/README.md
+++
@@ -1,3 +0,0 @@
-# flutter_daftar_karyawan
-
-A new Flutter project.
--- flutter_daftar_karyawan/android/app/src/main/kotlin/com/example/flutter_daftar_karyawan/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_daftar_karyawan
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
